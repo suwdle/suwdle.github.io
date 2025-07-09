caba91fffaa823e19d7591c5f1afd3d0354cfa9c
suwdle <suwdle1917@gmail.com>
2025-07-09 21:48:08 +0900

    UPDATE page link

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 item.innerHTML = `
                     <h3>${project.title}</h3>
                     <p>${project.description}</p>
-                    <a href="${project.link}" target="_blank">View Project</a>
+                    <a href="${project.link}">View Project</a>
                 `;
                 projectList.appendChild(item);
             });
